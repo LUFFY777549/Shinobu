274109a2a823e8a3089bfc62965a9b1bf51be2e0
LUFFY777549 <dragonsimagine1212@gmail.com>
2025-10-23 08:11:14 +0530

    Update start.py

--- Shinobu/Modules/start.py
+++ Shinobu/Modules/start.py
@@ -4,8 +4,6 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from Shinobu import bot
 from Shinobu.db import Users, Groups
 
-group_filter = filters.chat_type.groups
-
 START_IMAGE = "https://files.catbox.moe/xa33dy.jpg"
 START_TIME = datetime.utcnow()
 
@@ -45,7 +43,6 @@ async def start_private(client, message):
     ])
 
     await client.send_photo(message.chat.id, START_IMAGE, caption=caption, reply_markup=buttons)
-
 
 @bot.on_message(filters.command("start") & filters.group)
 async def start_group(client, message):
