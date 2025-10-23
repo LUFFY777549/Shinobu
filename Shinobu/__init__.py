7763878a67d0f6c532b507939b14dc02ee059da7
LUFFY777549 <dragonsimagine1212@gmail.com>
2025-10-23 12:57:15 +0530

    Update __init__.py

--- Shinobu/__init__.py
+++ Shinobu/__init__.py
@@ -1,17 +1,1 @@
-from pyrogram import Client
-from config import API_ID, API_HASH, BOT_TOKEN
-import logging
-
-bot = Client(
-    "ShinobuMusic",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN,
-    plugins=dict(root="Shinobu/Modules")
-)
-
-logging.basicConfig(
-  format="[KuroAI-Beta] %(name)s - %(levelname)s - %(message)s",
-  handlers=[logging.FileHandler("logs.txt"), logging.StreamHandler()],
-  level=logging.INFO,
-)
+# __init__.py
