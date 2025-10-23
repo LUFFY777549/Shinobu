b188e5bce4eb5bd25e2d903c4f3c67539cf1d614
LUFFY777549 <dragonsimagine1212@gmail.com>
2025-10-23 13:00:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,3 +3,5 @@ import os
 API_ID = int(os.environ.get("API_ID", "21218274"))
 API_HASH = os.environ.get("API_HASH", "3474a18b61897c672d315fb330edb213")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "8032028233:AAFhbKcOr6LFjkmz2SNbrCKSoj6BLu1LF6I")
+STRING_SESSION = os.environ.get("STRING_SESSION", "")
+YR_COOKIES = os.environ.get("YR_COOKIES", "")
