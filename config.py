e2861b7fd6ac50c4c3501be27ec3da5d0d0e9fbe
LUFFY777549 <dragonsimagine1212@gmail.com>
2025-10-23 07:47:48 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+API_ID = int(os.environ.get("API_ID", "21218274"))
+API_HASH = os.environ.get("API_HASH", "3474a18b61897c672d315fb330edb213")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8032028233:AAFhbKcOr6LFjkmz2SNbrCKSoj6BLu1LF6I")
