856d184942254af2ed80853e1a03d627bc545a77
LUFFY777549 <dragonsimagine1212@gmail.com>
2025-10-26 16:31:16 +0530

    Update __init__.py

--- Shinobu/__init__.py
+++ Shinobu/__init__.py
@@ -1,1 +1,18 @@
 # __init__.py
+from pyrogram import Client
+from config import API_ID, API_HASH, BOT_TOKEN
+import logging
+
+bot = Client(
+    "ShinoBuBot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="Shinobu/Modules")
+)
+
+logging.basicConfig(
+  format="[Alpha-Bot] %(name)s - %(levelname)s - %(message)s",
+  handlers=[logging.FileHandler("logs.txt"), logging.StreamHandler()],
+  level=logging.INFO,
+)
