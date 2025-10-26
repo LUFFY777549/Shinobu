ea3b495bfbf5d290f71ceac584403eae01702d88
LUFFY777549 <dragonsimagine1212@gmail.com>
2025-10-26 16:45:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 
 API_ID = int(os.environ.get("API_ID", "21218274"))
 API_HASH = os.environ.get("API_HASH", "3474a18b61897c672d315fb330edb213")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8032028233:AAFhbKcOr6LFjkmz2SNbrCKSoj6BLu1LF6I")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8286962332:AAG_pSsOPUNCGLKut7hsUK7yCLw8yoQzArk")
